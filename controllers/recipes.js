d41b75acad02720e9ea6cef0a5b5089fad907213
fjalafoo <fjalafoo@gmail.com>
2022-10-30 17:44:45 +0300

    comments module

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -11,20 +11,47 @@ const bcrypt = require('bcrypt')
 
   //GET /recipes/id ==> it will display recipe details
 
-  router.get('/:id', (req, res) => {
+  router.get('/:id', async(req, res) => {
 
     let mealId = req.params.id
     // console.log(mealId)
     let recipeUrl = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
     // Use request to call the API
-  axios.get(recipeUrl).then(apiResponse => {
-    
+  axios.get(recipeUrl).then( async apiResponse => {
+    let comments = await db.comment.findAll({
+      where: {recipeId: mealId},
+      include: [db.user]
+    })
     let recipeDetail = apiResponse.data;
     // console.log(recipeDetail)
-    res.render('recipes/recipeDetail.ejs', {recipeDetail:recipeDetail})
+    res.render('recipes/recipeDetail.ejs', {recipeDetail:recipeDetail,comments:comments})
   })
 
   })
+
+
+//post comments
+router.post('/:id', async (req,res)=>{
+  if(res.locals.user){
+  db.comment.create({
+      comment:req.body.comment,
+      recipeId:req.params.id,
+      userId:res.locals.user.dataValues.id,
+      include: [db.user, db.recipe]
+  })
+  .then((post) => {
+      res.redirect(`/recipes/${req.params.id}`)
+
+  })
+
+  .catch((error)=>{
+      console.log(error)
+  })
+  }else {
+    res.send("kindly login.") 
+    
+  }
+})
 
 
 // GET /recipes/random display a random recipe
